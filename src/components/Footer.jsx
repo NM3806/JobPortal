c63c890d1dd75472b2500baa172ea4544dda1d08
Naitik Madarwal <naitikmadarwal3806@gmail.com>
2024-10-30 17:10:47 +0530

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,11 @@ function Footer() {
     <footer id='contact' className="bg-blue-600 py-6 text-white text-center px-4 md:px-8">
       <p>&copy; {new Date().getFullYear()} JobPortal. All rights reserved.</p>
       <div className="mt-2 flex justify-center space-x-4">
-        <a href="#" className="hover:text-blue-200">
+        <a href="https://www.linkedin.com/in/naitik-madarwal-nm3806/" className="hover:text-blue-200">
           <FaLinkedin size={24} />
         </a>
         <span>|</span>
-        <a href="#" className="hover:text-blue-200">
+        <a href="https://github.com/NM3806" className="hover:text-blue-200">
           <FaTwitter size={24} />
         </a>
       </div>
